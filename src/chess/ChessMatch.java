860bdb057514b241ec1ae904729926cddfa11a6d
Andre Santana <andrelusantana@hotmail.com>
2019-06-26 21:45:37 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -50,6 +50,9 @@ public class ChessMatch { // classe de definição da partida de xadrez
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position.");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece.");
+		}
 		
 	}
 
